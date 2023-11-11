43e4f7585a57f8b4f75d1a2cd00ff1807929b941
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-11-11 10:56:59 +0200

    chess pieces moves script

---
+++ chess_pieces_moves.py
@@ -0,0 +1,11 @@
+ChessPieces:
+	def getRookPossibleMoves(self, current_box):
+		pass
+	def getKnightPossibleMoves(self, current_box):
+		pass
+	def getBishopPossibleMoves(self, current_box):
+		pass
+	def getQueenPossibleMoves(self, current_boc):
+		pass
+	def getKingPossibleMoves(self, current_box):
+		pass
